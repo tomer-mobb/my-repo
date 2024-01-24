cd18c319beac0af5378e841841aef3058324645a
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2024-01-24 18:55:04 +0700

    Update 3423.js

--- 3423.js
+++ 3423.js
@@ -7,4 +7,4 @@ var unsafe_div = window.document.getElementById("vulnerable-div");
 unsafe_div.innerHTML = "Hello to you " + username;
 
 var unsafe_div2 = window.document.getElementById("vulnerable-div2");
-unsafe_div2.textContent = "Hello to you " + username;
+unsafe_div2.innerHTML = "Hello to you " + username;
