17efbbaddc37786f1b36c82e4238dbef181db0ac
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2024-01-24 18:54:59 +0700

    Update 3423.js

--- 3423.js
+++ 3423.js
@@ -4,7 +4,7 @@ var urlParams = new URLSearchParams(window.location.search);
 var username = urlParams.get('username');
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.textContent = "Hello to you " + username;
+unsafe_div.innerHTML = "Hello to you " + username;
 
 var unsafe_div2 = window.document.getElementById("vulnerable-div2");
 unsafe_div2.textContent = "Hello to you " + username;
